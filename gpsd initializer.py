d0fd9fd232fd8e5ae60203cad9ba5e9f82485a10
ardymarky <aqmarkin@gmail.com>
2023-11-14 21:37:11 +0000

    backups

--- gpsd initializer.py
+++ gpsd initializer.py
@@ -1,11 +1,11 @@
 import os
 import subprocess
 
-subprocess.Popen(["killall", "-9", "gpsd", "ntpd"])
-subprocess.Popen(["gpsd", "-n", "/dev/ttyUSB"])
-subprocess.Popen(["sleep", "2"])
-subprocess.Popen(["ntpd", "-gN"])
-subprocess.Popen(["sleep", "2"])
-subprocess.Popen(["cgps"])
+subprocess.run(["killall", "-9", "gpsd", "ntpd"])
+subprocess.run(["gpsd", "-n", "/dev/ttyUSB0"])
+subprocess.run(["sleep", "2"])
+subprocess.run(["ntpd", "-gN"])
+subprocess.run(["sleep", "2"])
+subprocess.run(["cgps"])
 
 
